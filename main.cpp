e66090aff7b384ef3072ffb3f0ed580508060e51
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-08-07 13:05:02 +0900

    Feat: Basic Window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+}
 
 int main()
 {
 	std::cout << "Hello Main." << std::endl;
+
+	const wchar_t* kClassName = L"GilgameshWindowClass";
+	HINSTANCE hInstance = GetModuleHandle(nullptr);
+
+	WNDCLASSEX wc = {};
+	wc.cbSize		 = sizeof(WNDCLASSEX);
+	wc.style		 = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc	 = WndProc;
+	wc.hInstance	 = hInstance;
+	wc.hCursor		 = LoadCursor(nullptr, IDC_ARROW);
+	wc.lpszClassName = kClassName;
+	RegisterClassEx(&wc);
+
+	RECT rc = { 0, 0, 1280 ,720 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND hwnd = CreateWindowEx(0, kClassName, L"Project Gilgamesh", WS_OVERLAPPEDWINDOW,
+							   CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,
+							   nullptr, nullptr, hInstance, nullptr);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	bool running = true;
+
+	while (running)
+	{
+		MSG msg;
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT) running = false;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+
+	}
+
+
 	return 0;
 }
