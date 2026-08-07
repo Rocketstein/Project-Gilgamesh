7ee8521945401b5467ab43625b18e89678e6ff70
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-08-08 01:35:32 +0900

    Resolve CMakeLists compile definition error; add Window::Create()

--- Engine/Platform/Windows/Window.cpp
+++ Engine/Platform/Windows/Window.cpp
@@ -1,1 +1,43 @@
 #include "Window.h"
+
+namespace
+{
+	LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		switch (uMsg)
+		{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+}
+
+bool Window::Create()
+{
+	const wchar_t* kClassName = L"GilgameshWindowClass";
+	HINSTANCE hInstance = GetModuleHandle(nullptr);
+
+	WNDCLASSEX wc = {};
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.lpszClassName = kClassName;
+	RegisterClassEx(&wc);
+
+	RECT rc = { 0, 0, 1280 ,720 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND hwnd = CreateWindowEx(0, kClassName, L"Project Gilgamesh", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,
+		nullptr, nullptr, hInstance, nullptr);
+	if (hwnd == nullptr) return false;
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	return true;
+}
